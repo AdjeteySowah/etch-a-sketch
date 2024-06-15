09fdbe7e2f26931b721e3173c3921d06ddb6b9f0
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2024-06-15 18:17:03 +0000

    Change bg-color of buttons when clicked

--- index.js
+++ index.js
@@ -1,42 +1,13 @@
 
-let gridCellsContainer = document.querySelector(".inner-container");
-
-function createDefaultGrids () {
-    for (let i = 1; i <= 400; i++) {
-        let gridCell = document.createElement("div");
-        gridCell.className = "grid-cell";
-        gridCell.style.cssText = `width: 24px;
-                                  height: 24px;`;
-        gridCellsContainer.appendChild(gridCell);
-    }
-
-    attachEventListeners();
-}
-
-createDefaultGrids();
-
-function attachEventListeners() {
-    let gridCellNodes = document.querySelectorAll(".grid-cell");
-    gridCellNodes.forEach((gridCellNode) => {
-        gridCellNode.addEventListener("mouseover", changeColor);
-    });
-}
-
-function changeColor(event) {
-    document.querySelectorAll(".grid-cell").forEach(gridCell => {
-        gridCell.classList.remove("new-color");
-    });
-
-    event.target.style.backgroundColor = colorInput.value;
-    event.target.classList.add("new-color");
-}
+    // || SIDEBAR
 
 let colorInput = document.querySelector("#color-picker");
 
-function updateGridBgColor() {
-    let colorMode = document.querySelector(".color-mode");
-    colorMode.style.backgroundColor = colorInput.value;
+let colorMode = document.querySelector(".color-mode");
 
+function updateBgColorAndCSS() {
+    colorMode.style.backgroundColor = colorInput.value;
+        // the rest of the code in this function does not necessarily assign the gridCells their bgcolor, instead it enforces that they keep a particular color(in the stylesheet) as their bgcolor. Their bgcolor is actually assigned to them in the color function.
     const styleSheet = document.styleSheets[0];
     let coloredGridCells = document.querySelectorAll(".new-color");
 
@@ -51,7 +22,107 @@ function updateGridBgColor() {
     }
 }
 
-colorInput.addEventListener("input", updateGridBgColor);
+colorInput.addEventListener("input", updateBgColorAndCSS);
+
+let buttons = Array.from(document.querySelectorAll(".sidebar-btn"));
+let buttonsNot1stOne = buttons.filter((button) => !(button.classList.contains("color-mode")));
+let buttonsNot2ndOne = buttons.filter((button) => !(button.classList.contains("rainbow-mode")));
+let buttonsNot3rdOne = buttons.filter((button) => !(button.classList.contains("eraser")));
+let buttonsNot4thOne = buttons.filter((button) => !(button.classList.contains("clear")));
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        if (button.classList.contains("color-mode")) {
+            for(let i = 0; i < buttonsNot1stOne.length; i++) {
+                buttonsNot1stOne[i].style.backgroundColor = "rgb(255, 255, 255)";
+                buttonsNot1stOne[i].style.color = "rgba(0, 0, 0, 0.7)";
+                buttonsNot1stOne[i].style.borderColor = "rgba(0, 0, 0, 0.7)";
+            }
+            event.target.style.backgroundColor = colorInput.value;
+            event.target.style.color = "rgb(255, 255, 255)";
+
+            colorOnMouseover();
+        } else if (button.classList.contains("rainbow-mode")) {
+            for(let i = 0; i < buttonsNot2ndOne.length; i++) {
+                buttonsNot2ndOne[i].style.backgroundColor = "rgb(255, 255, 255)";
+                buttonsNot2ndOne[i].style.color = "rgba(0, 0, 0, 0.7)";
+                buttonsNot2ndOne[i].style.borderColor = "rgba(0, 0, 0, 0.7)";
+            }
+            event.target.style.backgroundColor = colorInput.value;
+            event.target.style.color = "rgb(255, 255, 255)";
+        } else if (button.classList.contains("eraser")) {
+            for(let i = 0; i < buttonsNot3rdOne.length; i++) {
+                buttonsNot3rdOne[i].style.backgroundColor = "rgb(255, 255, 255)";
+                buttonsNot3rdOne[i].style.color = "rgba(0, 0, 0, 0.7)";
+                buttonsNot3rdOne[i].style.borderColor = "rgba(0, 0, 0, 0.7)";
+            }
+            event.target.style.backgroundColor = colorInput.value;
+            event.target.style.color = "rgb(255, 255, 255)";
+
+            decolorizeOnMouseover();
+        } else {
+            for(let i = 0; i < buttonsNot4thOne.length; i++) {
+                buttonsNot4thOne[i].style.backgroundColor = "rgb(255, 255, 255)";
+                buttonsNot4thOne[i].style.color = "rgba(0, 0, 0, 0.7)";
+                buttonsNot4thOne[i].style.borderColor = "rgba(0, 0, 0, 0.7)";
+            }
+            event.target.style.backgroundColor = colorInput.value;
+            event.target.style.color = "rgb(255, 255, 255)";
+        } 
+    });
+});
+
+function decolorize(event) {
+    let gridCells = document.querySelectorAll(".grid-cell");
+    gridCells.forEach((gridCell) => {
+        gridCell.classList.remove("new-color");
+    });
+
+    event.target.style.backgroundColor = "rgb(255, 255, 255)";
+}
+
+function decolorizeOnMouseover() {
+    let gridCells = document.querySelectorAll(".grid-cell");
+    gridCells.forEach((gridCell) => {
+        gridCell.addEventListener("mouseover", decolorize);
+    });
+}
+
+
+    // || CONTAINER
+
+let gridCellsContainer = document.querySelector(".inner-container");    
+
+function color(event) {
+    let gridCells = document.querySelectorAll(".grid-cell");
+    gridCells.forEach((gridCell) => {
+        gridCell.classList.remove("new-color");
+    });
+
+    event.target.style.backgroundColor = colorInput.value;
+    event.target.classList.add("new-color");
+}
+
+function colorOnMouseover() {
+    let gridCells = document.querySelectorAll(".grid-cell");
+    gridCells.forEach((gridCell) => {
+        gridCell.addEventListener("mouseover", color);
+    });
+}    
+    
+function createDefaultGrids () {
+    for (let i = 1; i <= 400; i++) {
+        let gridCell = document.createElement("div");
+        gridCell.className = "grid-cell";
+        gridCell.style.cssText = `width: 24px;
+                                  height: 24px;`;
+        gridCellsContainer.appendChild(gridCell);
+    }
+
+    // colorOnMouseover();
+}
+
+createDefaultGrids();
 
 let rangeLabel = document.querySelector(".grid-cell-range");
 let range = document.querySelector("#grid-cell-range");
@@ -107,7 +178,10 @@ function updateRGN () {
         }
     }
 
-    attachEventListeners();
+    // colorOnMouseover();
 }
+
+
+// || CROSS-SECTIONAL IMPACT
 
 range.addEventListener("input", updateRGN);
